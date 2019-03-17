30320f4fcf147aceed67f1af1d0277e5d0b0bd7a
Johan Samarxhi <jsamarxhi@gmail.com>
2019-03-17 15:01:45 -0400

    - Enabled the audio toggle buttons to work in the main menu

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -8,24 +8,76 @@ using UnityEngine.SceneManagement;
  **/
 public class MenuController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{	
-		// Currently just waits for a tap from the user to begin the game
-//		for (int i = 0; i < Input.touchCount; ++i) 
-//		{
-//			if (Input.GetTouch (0).phase.Equals (TouchPhase.Began)) 
-//			{
-//				SceneManager.LoadScene (1);
-//			}
-//		}
+	public GameObject MainMenuMusic;
+	public GameObject MenuClickAudio;
 
-		if(Input.touchCount > 0)
-			SceneManager.LoadScene (1);
+	private int isMuteMusic;
+	private int isMuteSoundEffects;
+	private float musicVolume;
+
+	private AudioSource musicAudioSource;
+	private AudioSource clickAudioSource;
+
+	// Use this for initialization
+	void Start () 
+	{
+		musicAudioSource = MainMenuMusic.GetComponent<AudioSource> ();
+		musicVolume = musicAudioSource.volume;
+
+		clickAudioSource = MenuClickAudio.GetComponent<AudioSource> ();
+
+
+		if (PlayerPrefs.HasKey ("isMuteMusic"))
+			isMuteMusic = PlayerPrefs.GetInt ("isMuteMusic");
+		else
+		{
+			PlayerPrefs.SetInt ("isMuteMusic", 0);
+			isMuteMusic = PlayerPrefs.GetInt ("isMuteMusic");
+		}
+
+		if (PlayerPrefs.HasKey ("isMuteSoundEffects"))
+			isMuteSoundEffects = PlayerPrefs.GetInt ("isMuteSoundEffects");
+		else
+		{
+			PlayerPrefs.SetInt ("isMuteSoundEffects", 0);
+			isMuteSoundEffects = PlayerPrefs.GetInt ("isMuteSoundEffects");
+		}
+
+	}
+
+	public void MuteMusic()
+	{
+//		isMuteMusic = !isMuteMusic;
+
+		isMuteMusic = 1;
+		PlayerPrefs.SetInt ("isMuteMusic", isMuteMusic);
+
+//		AudioListener.volume = 0;
+		musicAudioSource.volume = 0;
+	}
+
+	public void UnMuteMusic()
+	{
+		isMuteMusic = 0;
+		PlayerPrefs.SetInt ("isMuteMusic", isMuteMusic);
+
+//		AudioListener.volume = 1;
+		musicAudioSource.volume = musicVolume;
+	}
+
+	public void MuteSoundEffects()
+	{
+		isMuteSoundEffects = 1;
+		PlayerPrefs.SetInt ("isMuteSoundEffects", isMuteSoundEffects);
+
+		clickAudioSource.volume = 0;
+	}
+
+	public void UnMuteSoundEffects()
+	{
+		isMuteSoundEffects = 0;
+		PlayerPrefs.SetInt ("isMuteSoundEffects", isMuteSoundEffects);
+
+		clickAudioSource.volume = 1;
 	}
 }
